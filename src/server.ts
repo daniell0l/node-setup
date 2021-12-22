9e68a2643e22981326482f70a533a83d096db164
daniel <daniel_l0l@hotmail.com>
2021-12-22 15:29:52 -0300

    Add Typescript and ts-node-dev

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import express, { response } from 'express'
+
+const app = express()
+
+app.get('/', (request, response) => {
+  return response.json({ message: 'Hello World' })
+})
+
+app.listen(3333, () => console.log('servidor rodando na porta 3333'))
